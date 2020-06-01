b3a202a9cc50699662142f868edb2dc75638fa56
20chan <2@0chan.dev>
2020-06-01 18:05:14 +0900

    temp web interface

--- kiki-viewer/Program.cs
+++ kiki-viewer/Program.cs
@@ -4,12 +4,14 @@ using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Blazor.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using kiki.shared;
 
 namespace kiki.viewer {
     public class Program {
         public static async Task Main(string[] args) {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            builder.Services.AddSingleton<KiKiDBContext>();
 
             await builder.Build().RunAsync();
         }
